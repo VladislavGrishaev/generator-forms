6e33b666f545ad2e681e627e861c8a27b1cb29c9
VladislavG <grishvm@yandex.ru>
2025-03-23 22:12:43 +0500

    изменена модель отображения формы, добавлены пару примеров форм

--- src/components/FormGenerator.vue
+++ src/components/FormGenerator.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-import {defineProps, defineModel, reactive} from 'vue';
+import {defineProps, reactive, watch} from 'vue';
 import '../assets/styles/form_generator.scss';
 
 /** -------------------------------------------------- **/
 
 const props = defineProps<{
+  title?: string;
   fields: {
     name: string;
     label: string;
@@ -17,12 +18,31 @@ const props = defineProps<{
 
 const emit = defineEmits(['update:modelValue', 'submit', 'cancel']);
 
+const model = reactive({
+  ...props.modelValue
+})
 
+watch(() => model, (newModel) =>
+    emit('update:modelValue', newModel),
+  {
+    deep: true
+  }
+)
+
+const onSubmit = () => {
+		emit('submit', model);
+}
+
+const onCancel = () => {
+		emit('cancel');
+}
 
 </script>
 
 <template>
-		<form class="form-generator" >
+		<form class="form-generator" @submit.prevent="onSubmit">
+
+				<h2 v-if="props.title">{{ props.title }}</h2>
 
 				<div
 								v-for="(field, index) in props.fields"
@@ -30,11 +50,12 @@ const emit = defineEmits(['update:modelValue', 'submit', 'cancel']);
 								class="form-generator__field"
 				>
 						<label
+										v-if="field.type !== 'checkbox'"
 										:for="field.name"
 										class="form-generator__label">{{ field.label }}
 						</label>
 						<input
-										v-if="field.type === 'text'"
+										v-if="['text', 'email', 'password', 'number'].includes(field.type)"
 										v-model="model[field.name]"
 										type="text"
 										:id="field.name"
@@ -74,19 +95,18 @@ const emit = defineEmits(['update:modelValue', 'submit', 'cancel']);
 										Согласен с условиями
 								</label>
 						</div>
-
-						<div class="form-generator__actions">
-								<button
-												type="button"
-												class="form-generator__button form-generator__button--cancel"
-												@click="onCancel"
-								>
-										Отмена
-								</button>
-								<button type="submit" class="form-generator__button form-generator__button--submit">
-										Сохранить
-								</button>
-						</div>
+				</div>
+				<div class="form-generator__actions">
+						<button
+										type="button"
+										class="form-generator__button form-generator__button--cancel"
+										@click="onCancel"
+						>
+								Отмена
+						</button>
+						<button type="submit" class="form-generator__button form-generator__button--submit">
+								Сохранить
+						</button>
 				</div>
 		</form>
 </template>
--- src/components/FormTabs.vue
+++ src/components/FormTabs.vue
@@ -1,14 +1,90 @@
 <script setup lang="ts">
 import FormGenerator from "./FormGenerator.vue";
+import {reactive} from "vue";
+
+const registerFields = [
+  {
+				name: 'name',
+				label: 'Имя',
+				type: 'text'
+		},
+		{
+				name: 'gender',
+				label: 'Пол',
+				type: 'select',
+				options: ['Мужской', 'Женский']
+		},
+		{
+				name: 'message',
+				label: 'Сообщение',
+				type: 'textarea'
+  },
+  {
+    name: 'agree',
+				label: 'Согласен с условиями',
+				type: 'checkbox'
+  }
+]
+
+const complainFields = [
+  {
+				name: 'name',
+				label: 'Имя',
+				type: 'text'
+  },
+		{
+    'name': 'complain',
+				'label': 'Жалоба',
+				'type': 'select',
+				'options': ['Жалоба 1', 'Жалоба 2']
+		}
+]
+
+const subscribeFields = [
+  {
+    name: 'email',
+    label: 'Почта',
+    type: 'email'
+  },
+  {
+    name: 'agree',
+    label: 'Согласен с условиями',
+    type: 'checkbox'
+  }
+];
 
 
+const formData = reactive({
+		name: '',
+		gender: '',
+		message: '',
+		agree: false
+})
 
 </script>
 
 <template>
 		<FormGenerator
+				:modelValue="formData"
+				title="Регистрация"
+				:fields="registerFields"
+				@submit="console.log('submit')"
+				@cancel="console.log('cancel')"
+		/>
 
+		<FormGenerator
+						:modelValue="formData"
+						:fields="complainFields"
+						title="Жалоба"
+						@submit="console.log('submit')"
+						@cancel="console.log('cancel')"
+		/>
 
-
+		<FormGenerator
+						:modelValue="formData"
+						:fields="subscribeFields"
+						title="Подписаться"
+						@submit="console.log('submit')"
+						@cancel="console.log('cancel')"
 		/>
 </template>
